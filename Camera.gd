f84576fc2e8fc17d19e954e813f04f03d02c82fd
Eugene Khutko <fobocaster@gmail.com>
2022-02-20 17:49:54 +0300

    Add zoom feature

--- Camera.gd
+++ Camera.gd
@@ -1,24 +1,30 @@
 extends Camera2D
 
+
+const ZOOM = [1, 2, 4]
+
 export var SPEED : = 500
 var default_position : = Vector2.ZERO
+var zoom_level : = 0
+
 
 func _process(delta: float) -> void:
-	var move_by : Vector2
+	var direction := Vector2.ZERO
 
-	if Input.is_action_pressed("ui_up"):
-		move_by = Vector2.UP * SPEED * delta
+	if Input.is_action_pressed("move_up"):
+		direction += Vector2.UP
 
-	if Input.is_action_pressed("ui_down"):
-		move_by = Vector2.DOWN * SPEED * delta
+	if Input.is_action_pressed("move_down"):
+		direction += Vector2.DOWN
 
-	if Input.is_action_pressed("ui_right"):
-		move_by = Vector2.RIGHT * SPEED * delta
+	if Input.is_action_pressed("move_right"):
+		direction += Vector2.RIGHT
 
-	if Input.is_action_pressed("ui_left"):
-		move_by = Vector2.LEFT * SPEED * delta
+	if Input.is_action_pressed("move_left"):
+		direction += Vector2.LEFT
 
-	position += move_by.round()
+	position += (direction.normalized() * SPEED * delta).round()
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	var motion_event := event as InputEventMouseMotion
@@ -29,5 +35,16 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_select"):
 		reset_position()
 
+	if event.is_action_pressed("zoom_out"):
+		zoom_level += 1
+
+	if event.is_action_pressed("zoom_in"):
+		zoom_level -= 1
+
+	zoom_level = clamp(zoom_level, 0, len(ZOOM) - 1)
+	zoom = Vector2(ZOOM[zoom_level], ZOOM[zoom_level])
+
+
 func reset_position() -> void:
 	position = default_position
+	zoom = Vector2.ONE
