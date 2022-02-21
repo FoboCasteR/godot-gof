db9fb7648666847a0ff558ba80b07ab733135a6c
Eugene Khutko <fobocaster@gmail.com>
2022-02-22 01:56:16 +0300

    Replace BitMap with arrays; Save number list as integer

--- Board.gd
+++ Board.gd
@@ -3,12 +3,15 @@ extends TileMap
 export(int) var alive_tile_id = 1
 export(int) var dead_tile_id = 0
 
-func draw_board(board: BitMap):
-	var size = board.get_size()
+func draw_board(board: Array):
+	var height := board.size()
+	var width := (board[0] as Array).size()
 
-	for row in size.y:
-		for col in size.x:
-			if board.get_bit(Vector2(col, row)):
-				set_cell(col, row, alive_tile_id)
+	for y in height:
+		var row := board[y] as Array
+
+		for x in width:
+			if row[x]:
+				set_cell(x, y, alive_tile_id)
 			else:
-				set_cell(col, row, dead_tile_id)
+				set_cell(x, y, dead_tile_id)
--- GameOfLife.gd
+++ GameOfLife.gd
@@ -1,14 +1,15 @@
 extends Node
 
-export(int, 3, 100) var width : = 10
-export(int, 3, 100) var height : = 10
+
+export(int, 3, 100) var width := 10
+export(int, 3, 100) var height := 10
 export(String) var rulestring := "B3/S23" setget set_rulestring
 
 signal next_generation(board)
 
-var boards: Array = []
-var birth_number_list: Array = [3]
-var survive_number_list: Array = [2, 3]
+var boards := []
+var birth_number_list := 0b0_0000_1000
+var survive_number_list := 0b0_0000_1100
 var neighbourhood: MooreNeighbourhood = MooreNeighbourhood.new()
 
 
@@ -23,21 +24,20 @@ class MooreNeighbourhood:
 		Vector2(1, 0),
 		Vector2(1, 1),
 	]
-	
-	func count(board: BitMap, coords: Vector2) -> int:
-		var border := board.get_size()
+
+	func count(board: Array, coords: Vector2, borders: Vector2) -> int:
 		var count := 0
 
 		for offset in OFFSETS:
 			var new_coords = coords + offset
 
-			if new_coords.y < 0 or new_coords.y >= border.y:
+			if new_coords.y < 0 or new_coords.y >= borders.y:
 				continue
 
-			if new_coords.x < 0 or new_coords.x >= border.x:
+			if new_coords.x < 0 or new_coords.x >= borders.x:
 				continue
 
-			if board.get_bit(new_coords):
+			if board[new_coords.y][new_coords.x]:
 				count += 1
 
 		return count
@@ -46,6 +46,7 @@ class MooreNeighbourhood:
 func _ready():
 	create_boards()
 
+
 func set_rulestring(value: String) -> void:
 	var regex = RegEx.new()
 	regex.compile("B(?<birth>.*)\/S(?<survive>.*)")
@@ -53,50 +54,60 @@ func set_rulestring(value: String) -> void:
 	var regex_match = regex.search(value)
 
 	if regex_match:
-		birth_number_list = []
-		survive_number_list = []
+		birth_number_list = 0
+		survive_number_list = 0
 		rulestring = value
 
 		for digit in regex_match.get_string("birth"):
-			birth_number_list.append(int(digit))
+			birth_number_list |= 1 << int(digit)
 
 		for digit in regex_match.get_string("survive"):
-			survive_number_list.append(int(digit))
-
+			survive_number_list |= 1 << int(digit)
 	else:
 		printerr('Invalid rulestring')
 
+
 func create_boards() -> void:
 	for i in 2:
-		var bitmap = BitMap.new()
-		bitmap.create(Vector2(width, height))
-		boards.append(bitmap)
+		var board := []
+
+		for j in height:
+			var row := []
+			row.resize(width)
+			board.append(row)
+
+		boards.append(board)
+
 
 func next_generation():
-	for row in height:
-		for col in width:
-			var coords = Vector2(col, row)
-			boards[1].set_bit(coords, next_cell_state(coords))
+	var front_board := boards[0] as Array
+	var back_board := boards[1] as Array
+	var borders := Vector2(width, height)
+
+	for y in height:
+		var row := front_board[y] as Array
+
+		for x in width:
+			var neighbor_count := neighbourhood.count(front_board, Vector2(x, y), borders)
+
+			if row[x]:
+				back_board[y][x] = survive_number_list & 1 << neighbor_count
+			else:
+				back_board[y][x] = birth_number_list & 1 << neighbor_count
+
 	boards.invert()
-	emit_signal("next_generation", boards[0])
 
-func next_cell_state(coords: Vector2) -> bool:
-	var neighbor_count := neighbourhood.count(boards[0], coords)
+	emit_signal("next_generation", front_board)
 
-	if boards[0].get_bit(coords):
-		return survive_number_list.has(neighbor_count)
-	else:
-		return birth_number_list.has(neighbor_count)
 
 func generate_soup():
-#	# Glider:
-#	for v in [Vector2(1, 0), Vector2(2, 1), Vector2(0, 2), Vector2(1, 2), Vector2(2, 2)]:
-#		boards[0].set_bit(v, true)
-
 	var h_boundary = range(floor(width * 0.33), ceil(width * 0.67))
 	var v_boundary = range(floor(height * 0.33), ceil(height * 0.67))
+	var front_board := boards[0] as Array
 
-	for row in h_boundary:
-		for col in v_boundary:
+	for y in h_boundary:
+		var row := front_board[y] as Array
+
+		for x in v_boundary:
 			if int(randf() > 0.8):
-				boards[0].set_bit(Vector2(col, row), true)
+				row[x] = true
