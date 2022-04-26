0035ddebbb1084295205b94db9e52817ccc81cb6
Eugene Khutko <fobocaster@gmail.com>
2022-04-26 23:16:03 +0300

    Randomize seed of rng

--- Main.gd
+++ Main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 func _ready() -> void:
+	randomize()
+
 	$GameOfLife.generate_soup()
 	$Board.draw_board($GameOfLife.boards[0])
 	$Camera.default_position = $Board.get_used_rect().size * $Board.cell_size / 2
